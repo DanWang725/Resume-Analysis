2a520118873cc9ace5a3064f6412d58b46536b13
DanWang725 <dw.smaug@gmail.com>
2022-01-08 13:26:39 -0500

    added pdf parsing to the file submission

--- Resume_App/Resume-Analysis_1a/app.js
+++ Resume_App/Resume-Analysis_1a/app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var path = require("path");
 const formidable = require('formidable');
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
 
 var routes = require("./routes");
 
@@ -38,4 +41,33 @@ app.post("/upload", function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(theFile);
   });
+})
+
+app.use(express.urlencoded({ extended: true }));
+
+//handling user submission and recieving of files
+app.post("/save", function(req, res){
+  console.log("BEGIN /save");
+  console.log(`req: ${JSON.stringify(req.body)}`);
+
+  let fileData = fs.readFileSync(req.body.filepond);
+
+  //parsing the pdf and extracting text
+  pdf(fileData).then(function(data) {
+
+    /* number of pages
+    console.log(data.numpages);
+    // number of rendered pages
+    console.log(data.numrender);
+    // PDF info
+    console.log(data.info);
+    // PDF metadata
+    console.log(data.metadata); 
+    // PDF.js version
+    // check https://mozilla.github.io/pdf.js/getting_started/
+    console.log(data.version);
+    // PDF text*/
+    console.log(data.text); 
+        
+});
 })
